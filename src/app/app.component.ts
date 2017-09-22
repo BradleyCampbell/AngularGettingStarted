13ed5ab1d9a07ae0ccd9a3522a81c7dfe4d55c3f
Brad Campbell <bradleyscampbell@outlook.com>
2017-09-22 19:15:16 -0400

    product service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
+import { ProductService } from './products/product.services';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ProductService]
 })
 export class AppComponent {
   pageTitle = 'Acme Product Management';
